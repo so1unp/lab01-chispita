9d2d8195d2f6a67df116ae979095cbbffae6b167
Nerea <toledonerea25@gmail.com>
2026-04-01 16:05:03 -0300

    Archivos de prueba

--- decrypt.c
+++ decrypt.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 #define BYTES_RUIDO 7 
-// El bloque total es ruido + el caracter real
 #define TAMANO_BLOQUE (BYTES_RUIDO + 1) 
 
 int main() {
@@ -16,16 +15,13 @@ int main() {
         while (total_leido < TAMANO_BLOQUE) {
             ssize_t n = read(0, buffer + total_leido, TAMANO_BLOQUE - total_leido);
 
-            if (n == 0) goto fin; // Fin del archivo
-            if (n < 0) {
+            if (n == 0) goto fin; 
                 perror("Error al leer");
                 exit(EXIT_FAILURE);
             }
             total_leido += n;
         }
         
-        // Escribimos el último byte del bloque (el real)
-        // Usamos el índice BYTES_RUIDO porque es la posición 7 (la octava)
         write(1, &buffer[BYTES_RUIDO], 1);
     }
 
